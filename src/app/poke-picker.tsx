c903dd00ffa7c6fda11fa074f30d6c2fbf2f0b98
Liam <{ID}+{username}@users.noreply.github.com>
2024-05-07 13:01:25 -0400

    fixed grass icon

--- src/app/poke-picker.tsx
+++ src/app/poke-picker.tsx
@@ -5,7 +5,6 @@ import { Button } from "flowbite-react";
 import pokemondata from "./pokemon-data.json";
 
 export default function PokePicker({ pokemonName, setPokemonName, pokemonData, setPokemonData, points, setPoints }: { pokemonName: string[], setPokemonName: React.Dispatch<React.SetStateAction<string[]>>, pokemonData: any[], setPokemonData: React.Dispatch<React.SetStateAction<any[]>>, points: number, setPoints: React.Dispatch<React.SetStateAction<number>>}) {
-
     const addPokemon = (pokemon: string, negPoints: number) => {
         if (pokemonName.length < 12 && points >= negPoints) {
             setPokemonName([...pokemonName, pokemon]);
