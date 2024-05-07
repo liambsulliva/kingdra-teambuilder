acba8f4262cdf770906f1eee2a676d8df733030d
Liam <{ID}+{username}@users.noreply.github.com>
2024-05-07 16:23:58 -0400

    implemented import/export

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,32 +10,27 @@ export default function Home() {
   const [pokemonName, setPokemonName] = useState<string[]>([]);
   const [pokemonData, setPokemonData] = useState<any[]>([]);
 
-  useEffect(() => {
-    //console.log(pokemonName);
-    const fetchPokemon = async (pokemon: string) => {
+  const updatePokemonData = async () => {
+    const updatedPokemonData = [];
+    for (const pokemon of pokemonName) {
       const formattedPokemon = pokemon.toLowerCase().replace(/\s/g, "-");
-      let sprite: string = "https://archives.bulbagarden.net/media/upload/8/8e/Spr_3r_000.png";
+      let sprite = "https://archives.bulbagarden.net/media/upload/8/8e/Spr_3r_000.png";
       try {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${formattedPokemon}/`);
         const data = await response.json();
-        //console.log(data);
-
         const response2 = await fetch(data.sprites.front_default);
         const data2 = await response2.blob();
-
         sprite = URL.createObjectURL(data2);
-
-        setPokemonData(prevData => [...prevData, { name: pokemon, type: data.types.map((type: any) => type.type.name), sprite, tera: false }]);
+        updatedPokemonData.push({ name: pokemon, type: data.types.map((type: any) => type.type.name), sprite, tera: false });
       } catch (error) {
-        setPokemonData(prevData => [...prevData, { name: pokemon, type: [], sprite, tera: false }]);
+        updatedPokemonData.push({ name: pokemon, type: [], sprite, tera: false });
       }
-      //console.log(pokemonData);
     }
-    pokemonName.forEach(pokemon => {
-      if (!pokemonData.some(p => p.name === pokemon)) {
-        fetchPokemon(pokemon);
-      }
-    });
+    setPokemonData(updatedPokemonData);
+  };
+
+  useEffect(() => {
+    updatePokemonData();
   }, [pokemonName]);
 
   return (
@@ -51,13 +46,35 @@ export default function Home() {
             points={points}
             setPoints={setPoints}
           />
-          <Button onClick={() => {
-            setPokemonData([]);
-            setPokemonName([]);
-            setPoints(120);
-          }} className="m-4 transition duration-200 active:scale-95">
-            Clear All
-          </Button>
+          <Button.Group className="m-auto my-2">
+            <Button onClick={() => {
+              const inputData = prompt("Enter your data: ");
+              if (inputData) {
+                setPokemonName(JSON.parse(inputData));
+                updatePokemonData();
+              } else {
+                alert("Invalid Input. Please try again.");
+              }
+            }}>
+              Import
+            </Button>
+            <Button onClick={() => {
+              setPokemonData([]);
+              setPokemonName([]);
+              setPoints(120);
+            }}>
+              Clear All
+            </Button>
+            <Button onClick={() => {
+                const data = JSON.stringify(pokemonName);
+                navigator.clipboard.writeText(data);
+                alert("Data Copied to Clipboard!");
+              }
+            }>
+              Export
+            </Button>
+          </Button.Group>
+          
         </div>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-4 m-4 flex-grow overflow-auto">
           {pokemonData.map((pokemon, index) => (
