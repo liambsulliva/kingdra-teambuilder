4b23bf087d68cbeff85f26fa2c538bb3de308b7d
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-06-19 11:56:01 +0200

    refactor folders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,8 @@ import Header from "./header";
 export default function Home() {
   return (
     <>
-      <div className="wrapper flex flex-col h-screen">
+      <div className="flex flex-col h-screen">
         <Header />
-      </div>
-      <div className="warning-message">
-        This website is only viewable in landscape mode.<br/>
-        Please rotate your device.
       </div>
     </>
   );
