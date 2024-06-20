171725b9de954e68cf972fe0d0dbb9f5db9499b3
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-06-20 13:13:56 +0200

    responsiveness for larger screens

--- src/components/PokeFinder.tsx
+++ src/components/PokeFinder.tsx
@@ -3,8 +3,8 @@ import PokeFinderCard from "./PokeFinderCard";
 
 export default function PokeFinder() {
     return (
-        <div className="grid xl:grid-cols-9 lg:grid-cols-6 grid-cols-3 gap-6 bg-[#f9f9f9] p-6 justify-center items-center rounded">
-            {Array(18).fill(<PokeFinderCard />)}
+        <div className="grid 2xl:grid-cols-12 xl:grid-cols-9 lg:grid-cols-6 sm:grid-cols-3 grid-cols-2 gap-6 bg-[#f9f9f9] p-6 justify-center items-center rounded">
+            {Array(36).fill(<PokeFinderCard />)}
         </div>
     );
 }
