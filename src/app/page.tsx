45aa02ad90e45faa457cc0ca393a7183ccad78fe
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-06-21 20:36:13 +0200

    typescript casing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 import PokeParty from "@/components/PokeParty";
 import PokeInfo from "@/components/PokeInfo";
 import PokeFinder from "@/components/PokeFinder";
