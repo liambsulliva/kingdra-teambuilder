8a53b5972a0f9ef0830565652cfcbce1f8b56b67
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-06-24 23:21:19 +0200

    fake EV calculator

--- src/components/StatBar.tsx
+++ src/components/StatBar.tsx
@@ -6,35 +6,52 @@ interface StatBarProps {
 }
 
 const StatBar: React.FC<StatBarProps> = ({ label, initialValue }) => {
-    const [value, setValue] = useState(initialValue);
+    const [additionalValue, setAdditionalValue] = useState(0);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setAdditionalValue(parseInt(event.target.value));
+    };
 
     useEffect(() => {
-        setValue(initialValue);
+        setAdditionalValue(0);
     }, [initialValue]);
+
+    const totalValue = initialValue + additionalValue;
 
     return (
         <div className='flex gap-4 items-center'>
-            <p className='text-gray-600'>{label}</p>
-            <div
-                style={{
-                    width: `${2 * value}px`,
-                    height: '5px',
-                    backgroundColor: `hsl(${value}, 100%, 50%)`,
-                    borderRadius: '15px',
-                    overflow: 'hidden',
-                    margin: '1rem 0'
-                }}
-            >
+            <p className='text-gray-600 text-nowrap'>{label}</p>
+            <div className='flex flex-col'>
                 <div
                     style={{
-                        height: '100%',
-                        width: `${value}%`,
-                        transition: 'width 0.5s ease-out, background-color 0.5s ease-out',
-                        backgroundSize: '300% 100%',
-                        backgroundPosition: `${100 - value}% 0`,
+                        width: `${1.25 * totalValue}px`,
+                        height: '5px',
+                        backgroundColor: `hsl(${totalValue / 1.25}, 100%, 50%)`,
+                        borderRadius: '15px',
+                        overflow: 'hidden',
+                        margin: '1rem 0'
                     }}
+                >
+                    <div
+                        style={{
+                            height: '100%',
+                            width: `${totalValue}%`,
+                            transition: 'width 0.5s ease-out, background-color 0.5s ease-out',
+                            backgroundSize: '300% 100%',
+                            backgroundPosition: `${100 - totalValue}% 0`,
+                        }}
+                    />
+                </div>
+                <input
+                    className='w-32'
+                    type="range"
+                    min="0"
+                    max="100"
+                    value={additionalValue}
+                    onChange={handleChange}
                 />
             </div>
+            <p>Total: {totalValue}</p>
         </div>
     );
 };
