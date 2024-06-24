5e9eb8212b5db28ebe7de3d2c8f7efd69ed311c9
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-06-24 23:03:37 +0200

    organize card layout

--- src/components/PokeInfo.tsx
+++ src/components/PokeInfo.tsx
@@ -48,27 +48,34 @@ export default function PokeInfo({ selectedPokemon }: { selectedPokemon: pokemon
                         className="w-40 h-40 mx-auto mb-6 object-contain"
                     />
                 )}
-                <p className="flex flex-row text-lg text-gray-600 mb-4">
-                    Type:
-                    <div className="px-2">
-                        <span className="font-semibold capitalize">{pokemonInfo.types[0].type.name}</span>
-                        {pokemonInfo.types[1] && (
-                            <span className="font-semibold capitalize"> / {pokemonInfo.types[1].type.name}</span>
-                        )}
+                <div className="flex justify-between">
+                    <p className="flex flex-row text-lg text-gray-600 mb-4">
+                        Type:
+                        <div className="px-2">
+                            <span className="font-semibold capitalize">{pokemonInfo.types[0].type.name}</span>
+                            {pokemonInfo.types[1] && (
+                                <span className="font-semibold capitalize"> / {pokemonInfo.types[1].type.name}</span>
+                            )}
+                        </div>
+                    </p>
+                    <div>
+                        <h3 className="text-xl font-bold text-gray-800 mb-3">Abilities:</h3>
+                        <ul className="space-y-2">
+                            {pokemonInfo.abilities.map((ability: any, index: number) => (
+                                <li key={index} className="text-gray-600 capitalize">{ability.ability.name}</li>
+                            ))}
+                        </ul>
                     </div>
-                </p>
-                <h3 className="text-xl font-bold text-gray-800 mb-3">Abilities:</h3>
-                <ul className="space-y-2">
-                {pokemonInfo.abilities.map((ability: any, index: number) => (
-                    <li key={index} className="text-gray-600 capitalize">{ability.ability.name}</li>
-                ))}
-                </ul>
-                <StatBar label={'HP'} initialValue={pokemonInfo.stats[0].base_stat} />
-                <StatBar label={'Atk'} initialValue={pokemonInfo.stats[1].base_stat} />
-                <StatBar label={'Def'} initialValue={pokemonInfo.stats[2].base_stat} />
-                <StatBar label={'Sp. Atk'} initialValue={pokemonInfo.stats[3].base_stat} />
-                <StatBar label={'Sp. Def'} initialValue={pokemonInfo.stats[4].base_stat} />
-                <StatBar label={'Speed'} initialValue={pokemonInfo.stats[5].base_stat} />
+                </div>
+                <div className="py-2">
+                    <StatBar label={'HP'} initialValue={pokemonInfo.stats[0].base_stat} />
+                    <StatBar label={'Atk'} initialValue={pokemonInfo.stats[1].base_stat} />
+                    <StatBar label={'Def'} initialValue={pokemonInfo.stats[2].base_stat} />
+                    <StatBar label={'Sp. Atk'} initialValue={pokemonInfo.stats[3].base_stat} />
+                    <StatBar label={'Sp. Def'} initialValue={pokemonInfo.stats[4].base_stat} />
+                    <StatBar label={'Speed'} initialValue={pokemonInfo.stats[5].base_stat} />
+                </div>
+                <a className="text-gray-500 hover:underline" target="_blank" href={`https://www.smogon.com/dex/sv/pokemon/${pokemonInfo.name}`}>Smogon Breakdown</a>
             </div>
             )}
         </div>
