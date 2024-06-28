fac838aed8d2bf54e5be11e454851ba051d07181
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-06-28 07:01:57 +0200

    remove clear from searchbar

--- src/components/PokeFinder.tsx
+++ src/components/PokeFinder.tsx
@@ -74,7 +74,7 @@ export default function PokeFinder({ setPokemonParty }: { setPokemonParty: React
             <div className="w-full flex md:justify-end justify-center pt-2 relative text-gray-600">
                 <input 
                     className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none" 
-                    type="search" 
+                    type="text" 
                     name="search" 
                     placeholder="Search" 
                     value={searchTerm}
