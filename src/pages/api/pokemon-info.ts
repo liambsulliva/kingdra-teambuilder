f7752c131337b0215e3f1197d86273e6f0756707
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-07-11 23:05:51 +0200

    optimize pokemonInfo api route by returning as much data as needed

--- src/pages/api/pokemon-info.ts
+++ src/pages/api/pokemon-info.ts
@@ -1,19 +1,31 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import type { pokemonInfo } from '../../../lib/pokemonInterface';
 import fetch from 'node-fetch';
 
 interface PokemonData {
-	abilities: Array<{
-		ability: {
-			url: string;
+	name: string;
+	id: number;
+	types: Array<{ type: { name: string } }>;
+	abilities: Array<{ ability: { name: string; url: string } }>;
+	sprites: {
+		versions: {
+			'generation-v': {
+				'black-white': {
+					animated: {
+						front_default: string | null;
+					};
+				};
+			};
 		};
-	}>;
+	};
+	stats: Array<{ base_stat: number }>;
+	moves: Array<{ move: { name: string; url: string } }>;
+	location_area_encounters: string;
 }
 
 interface AbilityData {
 	effect_entries: Array<{
-		language: {
-			name: string;
-		};
+		language: { name: string };
 		effect: string;
 	}>;
 }
@@ -37,23 +49,34 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
 			// Fetch ability details
 			const abilitiesWithEffects = await Promise.all(
-				data.abilities.map(async (ability: { ability: { url: string } }) => {
+				data.abilities.map(async (ability) => {
 					const abilityResponse = await fetch(ability.ability.url);
 					const abilityData = (await abilityResponse.json()) as AbilityData;
 					const effect =
 						abilityData.effect_entries.find(
-							(entry: { language: { name: string } }) =>
-								entry.language.name === 'en'
+							(entry) => entry.language.name === 'en'
 						)?.effect || 'No English description available';
-					return { ...ability, effect };
+					return {
+						ability: { name: ability.ability.name },
+						effect,
+					};
 				})
 			);
 
-			// Replace the original abilities with the updated ones
-			data.abilities = abilitiesWithEffects;
+			// Construct the pokemonInfo object
+			const pokemonInfo: pokemonInfo = {
+				name: data.name,
+				id: data.id,
+				types: data.types,
+				abilities: abilitiesWithEffects,
+				sprites: data.sprites,
+				stats: data.stats,
+				moves: data.moves,
+				location_area_encounters: data.location_area_encounters,
+			};
 
-			console.log(data);
-			res.status(200).json(data);
+			console.log(pokemonInfo);
+			res.status(200).json(pokemonInfo);
 		} catch (error) {
 			console.error(error);
 			res.status(500).json({ message: 'Internal Server Error' });
