73aa53cbf045b9a50a92333b4d6a7bdbcb3745e8
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-10-22 20:55:50 -0400

    add service worker (for real)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -26,7 +27,10 @@ const RootLayout = ({
 					rel='stylesheet'
 				></link>
 			</head>
-			<body>{children}</body>
+			<body>
+				{children}
+				<Script src='/service-worker.js' />
+			</body>
 		</html>
 	);
 };
