ebd9328eee826bd2d086cb1693e9e46009d98a3a
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-10-22 21:27:26 -0400

    removing viewport mods

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,10 +17,6 @@ const RootLayout = ({
 			<head>
 				<link rel='icon' href='/favicon.ico' />
 				<link rel='preconnect' href='https://fonts.googleapis.com' />
-				<meta
-					name='viewport'
-					content='width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'
-				/>
 				<link
 					rel='preconnect'
 					href='https://fonts.gstatic.com'
