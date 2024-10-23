be6fb27ca3fc5364d4e1fa87a7ea0cf7574e6fb0
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-10-22 21:24:24 -0400

    disable scroll on PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,16 +16,20 @@ const RootLayout = ({
 		<html lang='en'>
 			<head>
 				<link rel='icon' href='/favicon.ico' />
-				<link rel='preconnect' href='https://fonts.googleapis.com'></link>
+				<link rel='preconnect' href='https://fonts.googleapis.com' />
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'
+				/>
 				<link
 					rel='preconnect'
 					href='https://fonts.gstatic.com'
 					crossOrigin={'anonymous'}
-				></link>
+				/>
 				<link
 					href='https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
 					rel='stylesheet'
-				></link>
+				/>
 			</head>
 			<body>
 				{children}
